0d772bac1c9f91351efdc12264f93e8e63053665
Roberto Paes <robertocpaes@gmail.com>
2023-05-02 06:28:40 -0300

    Corrigindo propriedades default para o autofill.

--- Models/ReceitaWSAutoFill.cs
+++ Models/ReceitaWSAutoFill.cs
@@ -2,8 +2,8 @@
 {
     public class ReceitaWSAutoFill
     {
-        public bool AutoFill { get; set; } = true
-        public int MinFill { get; set; } = 1000
-        public int MaxFill { get; set; } = 2000
+        public bool AutoFill { get; set; } = true;
+        public int MinFill { get; set; } = 1000;
+        public int MaxFill { get; set; } = 2000;
     }
 }
